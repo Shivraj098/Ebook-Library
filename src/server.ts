17ba0e2eda323c4c13f3e8ac758d2dfbc6da2f98
Shivraj <rajshivraj5555@gmail.com>
2025-10-07 22:47:17 +0530

    Debugged issues relating to nodemon and typescript

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
