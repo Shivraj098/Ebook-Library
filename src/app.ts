f235671c0831bdcbc2151a2dadbda0867c57f5fe
Shivraj <rajshivraj5555@gmail.com>
2025-10-11 21:59:40 +0530

    setup multer

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express from "express";
 
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 const app = express();
 
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to eLib API" });
 });
-
+app.use("/api/books",bookRouter)
 
 app.use("/api/users",userRouter)
 app.use(globalErrorHandler);
--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 const createBook = (req: Request, res: Response, next: NextFunction) => {
+    
     res.json({ message: "Book created" });
 };
 export default createBook;
