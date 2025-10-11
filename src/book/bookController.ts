b5c1e4951b94237326730be9b2a24cedc3962f85
Shivraj <rajshivraj5555@gmail.com>
2025-10-11 21:24:16 +0530

    Add book model

---
+++ src/book/bookController.ts
@@ -0,0 +1,5 @@
+import { Request, Response, NextFunction } from "express";
+const createBook = (req: Request, res: Response, next: NextFunction) => {
+    res.json({ message: "Book created" });
+};
+export default createBook;
