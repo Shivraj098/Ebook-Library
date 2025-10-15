f7645313f8b5345146d1b96f913c52018c6ac5ed
Shivraj <rajshivraj5555@gmail.com>
2025-10-15 19:47:50 +0530

    Add cloudinary config

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 const createBook = (req: Request, res: Response, next: NextFunction) => {
     
+    const files = req.files as {[fieldname:string]: Express.Multer.File[]};
+    const coverImageMimeType = files.coverImage[0];
+    const uploadResult = await cloudinary.uploader.upload(filePath,){
+        filename_override:filename,
+        folder:"book-covers",
+        format:coverImageMimeType,
+    }
     res.json({ message: "Book created" });
 };
 export default createBook;
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,13 @@
 import dotenv  from 'dotenv'
+
 dotenv.config();
 const _config = {
   port: process.env.PORT,
   databaseURL:process.env.MONGO_URI,
   env:process.env.NODE_ENV,
   jwtSecret:process.env.JWT_SECRET,
+  cloudinaryCloudName:process.env.CLOUDINARY_CLOUD,
+  cloudinaryApiKey:process.env.CLOUDINARY_APIKEY,
+  cloudinaryApiSecret:process.env.CLOUDINARY_APISECRET,
 };
 export const config = Object.freeze(_config);
